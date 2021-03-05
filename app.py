47f463837013b00b6570e806356d587e8fac9d40
Shivam Mishra <66585128+shivam0201@users.noreply.github.com>
2021-03-05 22:34:40 +0530

    Login page for the recruiter and database authentication
    This is an additional modification to the previous code. Here, both user and recruiter could be authenticated and it works fine. I made use of single database with 2 collections for developer and employer respectively. The code is well organized and modified so it must not be manipulated

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/home')
-def index():
+def home():
     return render_template('index.html')
 
 
@@ -21,35 +21,72 @@ def user_profile():
     return render_template('profile_page.html')
 
 
-@app.route('/login')
-def employee():
+@app.route('/login_developer')
+def login_developer():
     return render_template('SIGNUP.html')
 
 
-@app.route('/login_validation', methods=['POST', 'GET'])
-def login_validation():
-    users = mongo.db.user
-    l_email = request.form.get('l_email')
-    l_passwd = request.form.get('l_password')
-    login_user = users.find_one({'username': l_email})
-    if login_user:
-        if users.find_one({'password': l_passwd}):
+@app.route('/login_recruiter')
+def login_recruiter():
+    return render_template('RECRUITER_SIGNUP.html')
+
+# <---------------------------------------section for the developer---------------------------------------------------------------------------->
+
+
+@app.route('/login_validation_developer', methods=['POST', 'GET'])
+def login_validation_developer():
+    users_developer = mongo.db.user
+    l_email = request.form.get('l_email_d')
+    l_passwd = request.form.get('l_password_d')
+    login_user_developer = users_developer.find_one({'username': l_email})
+    if login_user_developer:
+        if users_developer.find_one({'password': l_passwd}):
             return redirect('/user_profile')
         return "Invalid password"
     return "No user found"
 
 
-@app.route('/add_user', methods=['POST'])
+@app.route('/add_user_developer', methods=['POST'])
+def add_user_developer():
+    if request.method == 'POST':
+        users_developer = mongo.db.user
+        existing_user_developer = users_developer.find_one(
+            {'username': request.form.get('r_email_d')})
+        if existing_user_developer is None:
+            r_email = request.form.get('r_email_d')
+            r_passwd = request.form.get('r_password_d')
+            users_developer.insert_one(
+                {'username': r_email, 'password': r_passwd})
+            return redirect('/login_developer')
+        return "The user already exists"
+
+
+# <------------------------------------------------Section for the recruiter---------------------------------------------------------------->
+
+@app.route('/login_validation_recruiter', methods=['POST', 'GET'])
+def login_validation():
+    users_r = mongo.db.employer
+    l_email = request.form.get('l_email_r')
+    l_passwd = request.form.get('l_password_r')
+    login_user_r = users_r.find_one({'username': l_email})
+    if login_user_r:
+        if users_r.find_one({'password': l_passwd}):
+            return "Recruiter successfully logged in"
+        return "Invalid password"
+    return "No user found"
+
+
+@app.route('/add_user_recruiter', methods=['POST'])
 def add_user():
     if request.method == 'POST':
-        users = mongo.db.user
-        existing_user = users.find_one(
-            {'username': request.form.get('r_email')})
-        if existing_user is None:
-            r_email = request.form.get('r_email')
-            r_passwd = request.form.get('r_password')
-            users.insert_one({'username': r_email, 'password': r_passwd})
-            return redirect('/login')
+        users_r = mongo.db.employer
+        existing_user_r = users_r.find_one(
+            {'username': request.form.get('r_email_r')})
+        if existing_user_r is None:
+            r_email = request.form.get('r_email_r')
+            r_passwd = request.form.get('r_password_r')
+            users_r.insert_one({'username': r_email, 'password': r_passwd})
+            return redirect('/login_recruiter')
         return "The user already exists"
 
 
