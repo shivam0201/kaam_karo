f84825dd2d10fd847c43db22daf4120e487cb999
Shivam Mishra <66585128+shivam0201@users.noreply.github.com>
2021-03-05 15:36:08 +0530

    Updated the correct app.py file

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import bcrypt
 app = Flask(__name__)
 
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/Employee'
+app.config['SECRET_KEY'] = 'development'
 
 mongo = PyMongo(app)
 
@@ -15,44 +16,41 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/user')
-def signup():
-    if 'login_username' in session:
-        return render_template('user_profile.html')
-    else:
-        return render_template('SIGNUP.html')
+@app.route('/user_profile')
+def user_profile():
+    return render_template('profile_page.html')
 
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    users = mongo.db.users
-    login_user = users.find_one({'email-id': request.form['login_email']})
+@app.route('/login')
+def employee():
+    return render_template('SIGNUP.html')
+
+
+@app.route('/login_validation', methods=['POST', 'GET'])
+def login_validation():
+    users = mongo.db.user
+    l_email = request.form.get('l_email')
+    l_passwd = request.form.get('l_password')
+    login_user = users.find_one({'username': l_email})
     if login_user:
-        if bcrypt.hashpw(request.form['login_password']) == login_user['password']:
-            session['username'] = request.form['login_email']
-            return render_template('user_profile.html')
+        if users.find_one({'password': l_passwd}):
+            return redirect('/user_profile')
+        return "Invalid password"
+    return "No user found"
 
-    return 'Invalid username/password'
 
-
-@app.route('/register', methods=['POST', "GET"])
-def register():
+@app.route('/add_user', methods=['POST'])
+def add_user():
     if request.method == 'POST':
         users = mongo.db.user
         existing_user = users.find_one(
-            {'email-id': request.form['register_email']})
-
+            {'username': request.form.get('r_email')})
         if existing_user is None:
-            hashpass = bcrypt.hashpw(
-                request.form['register_password'], bcrypt.gensalt())
-            users.insert(
-                {'email-id': request.form['register_email'], 'password': hashpass})
-            session['username'] = request.form['register_email']
-            return render_template('SIGNUP.html')
-
-        return 'The user already exists'
-
-    return render_template("SIGNUP.html")
+            r_email = request.form.get('r_email')
+            r_passwd = request.form.get('r_password')
+            users.insert_one({'username': r_email, 'password': r_passwd})
+            return redirect('/login')
+        return "The user already exists"
 
 
 if __name__ == '__main__':
